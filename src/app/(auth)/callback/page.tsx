c7c3723368c0620a4c84ea6fa697200b876e6a57
Arnold <luizarnoldch@gmail.com>
2024-03-16 00:59:51 -0500

    ath + middleware

--- src/app/(auth)/callback/page.tsx
+++ src/app/(auth)/callback/page.tsx
@@ -1,19 +1,27 @@
 "use client";
+
 import "aws-amplify/auth/enable-oauth-listener";
 import { fetchUserAttributes } from "aws-amplify/auth";
 import { Hub } from "aws-amplify/utils";
 import { useState, useEffect } from "react";
 // import useAuth from "@/lib/auth-store";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { LoaderCircle } from "lucide-react";
 
 export default function Callback() {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
   // const setUserAuth = useAuth((state) => state.setUserAuth);
+  const searchParams = useSearchParams()
+  const code = searchParams.get('code')
 
   useEffect(() => {
+    if(!code) {
+      router.push("/");
+    }
+
     const unsubscribe = Hub.listen("auth", async ({ payload }) => {
+      console.log(payload.event)
       switch (payload.event) {
         case "signInWithRedirect":
           const userAttributes = await fetchUserAttributes();
@@ -42,7 +50,7 @@ export default function Callback() {
       }
     });
     return unsubscribe;
-  }, [loading]);
+  }, [loading, code]);
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
